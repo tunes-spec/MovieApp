43a83df754c42240dbb036f47d0bd9fb6bbc89d0
高杉恒寛 <takasugi.tsunehiro@gmail.com>
2021-03-21 12:56:54 +0900

    no message

---
+++ LottieSamlple2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottieSamlple2
+//
+//  Created by TSUNE on 2021/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
