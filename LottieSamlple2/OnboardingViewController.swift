684ffe8eac0accf736e79b4e86ba568b56362191
高杉恒寛 <takasugi.tsunehiro@gmail.com>
2021-03-22 13:36:36 +0900

    part1

---
+++ LottieSamlple2/OnboardingViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  LottieSamlple2
+//
+//  Created by TSUNE on 2021/03/21.
+//
+
+import UIKit
+class OnboardingViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView:UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView.collectionViewLayout = layout
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.isPagingEnabled = true
+    }
+}
+
+extension OnboardingViewController:UICollectionViewDelegate {
+    
+}
+
+extension OnboardingViewController:UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemWidth = collectionView.bounds.width
+        let itemHeight = collectionView.bounds.height
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+extension OnboardingViewController:UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+        var color = UIColor.clear
+        if indexPath.item % 2 == 0 {
+            color = .red
+        } else {
+            color = .blue
+        }
+        cell.backgroundColor = color
+        return cell
+    }
+}
+
+
